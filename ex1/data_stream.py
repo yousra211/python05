19d7890924600ff1865efc41f09c4abeb557be25
yousra211 <hamdaouiya2110@gmail.com>
2026-03-18 01:50:52 +0000

    kwkw

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Any, List, Dict, Union, Optional
 
 class DataStream(ABC):
-    def __init__(self, stream_id: int, type:str, type:str):
+    def __init__(self, stream_id: str, type:str):
         self.stream_id = stream_id
         self.type = type
 
@@ -18,19 +18,54 @@ class DataStream(ABC):
 
 
 class SensorStream(DataStream):
-    def __init__(self, stream_id: int, type:str):
-        super().__init__(stream_id)
-
+    def __init__(self, stream_id: str, type:str):
+        super().__init__(stream_id, type)
+    def process_batch(self, data_batch: List[Any]) -> str:
+        print("Initializing Sensor Stream...")
+        print(f"Stream ID: {self.stream_id}, Type: {self.type}")
+        formatted = [f"{k}:{v}" for d in data_batch for k, v in d.items()]
+        return f"Processing sensor batch: {formatted}"
+    
 
 class TransactionStream(DataStream):
-    def __init__(self, stream_id: int, type:str):
-        super().__init__(stream_id)
+    def __init__(self, stream_id: str, type:str):
+        super().__init__(stream_id, type)
+    def process_batch(self, data_batch: List[Any]) -> str:
+        print("Initializing Transaction Stream...")
+        print(f"Stream ID: {self.stream_id}, Type: {self.type}")
+        formatted = [f"{k}:{v}" for d in data_batch for k, v in d.items()]
+        return f"Processing transaction batch: {formatted}"
 
 
 class EventStream(DataStream):
-    def __init__(self, stream_id: int, type:str):
-        super().__init__(stream_id)
+    def __init__(self, stream_id: str, type:str):
+        super().__init__(stream_id, type)
+    def process_batch(self, data_batch: List[Any]) -> str:
+        print("Initializing Event Stream...")
+        print(f"Stream ID: {self.stream_id}, Type: {self.type}")
+        return f"Processing event batch: {data_batch}"
 
 
 class StreamProcessor:
-    pass
+    def __init__(self):
+        self.streams: List[tuple] = []
+
+    def add_stream(self, stream:DataStream, dataa):
+        self.streams.append((stream, dataa))
+
+    def proccess_all(self):
+        for stream, data in self.streams:
+            text = stream.process_batch(data)
+            print(text)
+
+
+sen = SensorStream("SENSOR_001", "Enrironmental Data")
+tran = TransactionStream("TRANS_001", "Financial Data")
+event = EventStream("EVENT_001", "System Events")
+
+pros = StreamProcessor()
+pros.add_stream(sen, [{"temp":22.5}, {"humidity":65}, {"pressure":1013}])
+pros.add_stream(tran, [{"buy":100}, {"sell":150}, {"buy":75}])
+pros.add_stream(event, ["login", "error", "logout"])
+
+pros.proccess_all()
